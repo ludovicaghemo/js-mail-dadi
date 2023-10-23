6013b85d126e6b3243f1f6bd081bb2180faa2cc4
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-10-23 14:53:42 +0200

    add: array + data collection

---
+++ js-mail/js/script.js
@@ -0,0 +1,5 @@
+const email = ["ludovica@booleancareers.it", "francesco@booleancareers.it", "marco@booleancareers.it", "giulia@booleancareers.it", "federica@booleancareers.it", "gianluca@booleancareers.it", "ettore@booleancareers.it", "filippo@booleancareers.it"]; 
+
+const userEmail = prompt("Hello user! What's your email address?");
+console.log(userEmail);
+
