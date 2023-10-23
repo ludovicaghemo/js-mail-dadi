d1c9e602202c0a264ee45f233596450a53d5463a
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-10-23 15:37:14 +0200

    add: program logic - for cycle and if

--- js-mail/js/script.js
+++ js-mail/js/script.js
@@ -3,3 +3,21 @@ const email = ["ludovica@booleancareers.it", "francesco@booleancareers.it", "mar
 const userEmail = prompt("Hello user! What's your email address?");
 console.log(userEmail);
 
+let allowed = false;
+
+for (let i = 0; i < email.length; i++) {
+    const curEmail = email[i];
+    //prenditi l'iesimo elemento di email
+
+    if(curEmail === userEmail) {
+        allowed = true;
+    }
+
+    console.log(curEmail, userEmail, allowed);
+}
+
+if(allowed === true) {
+    console.log("Access allowed");
+} else {
+    console.log("Access denied");
+}
